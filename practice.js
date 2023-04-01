1d74077b2c1bcc96bc6445c6328787e37b25aff3
Khadir | FsDevKD <125905976+FSDevKD@users.noreply.github.com>
2023-04-01 19:24:44 -0400

    push

---
+++ practice.js
@@ -0,0 +1,62 @@
+
+// Declared 5 variables
+
+let khadir = "Name"; // variables
+let house = "Home"; //variables
+let truck = "Dodge"; //variables
+let job = "Rich"; //variables
+let fit = "Nice"; //variables 
+
+house = "Mansion"; // reassigned variable
+
+let person = ["Job","Location","Pay"]
+
+
+for ( let i = 0; i < num; i++ ) {
+    console.log(i)
+}
+
+for ( let i = -; i < num; oi++ ){
+    console.log(i)
+}
+
+for ( let i = 0; i < num; i++){
+    console.log(i)
+}
+
+for ( let i = 0; i < num; i++ ){
+    console.log(i)
+}
+
+for ( let i = 0; i < num; i++ ){
+    console.log(i)
+}
+
+for ( let i = 0; i < num; i++ ){
+    console.log(i)
+}
+
+let stopwatch = {}
+
+stopwatch.currentTime = 12
+
+stopwatch.tellTime = function(time){
+    console.log("the current time is ${time}")
+}
+
+stopwatch.tellTime(stopwatch.currentTime)
+
+// Objects 
+
+Constructor and classes 
+
+class MakeCar {
+    constructor(carMake, carModel, carColor, numOfdoors){
+        this.make = carMake;
+        this.model = carModel;
+        this.carColor = carColor;
+        this.numOfdoors = numOfdoors;
+
+    }
+    
+}

8403601ddfeba389ff8e1e94a8da165ade02aca5
Khadir | FsDevKD <125905976+FSDevKD@users.noreply.github.com>
2023-04-04 09:51:05 -0400

    Daily Push

---
+++ hero.js
@@ -0,0 +1,16 @@
+/*
+
+A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return True if yes, False otherwise :)
+
+https://www.codewars.com/kata/59ca8246d751df55cc00014c/
+
+*/
+
+function hero(bullets, dragons){
+    
+    return bullets >= dragons * 2; // check to see if number of bullets is greater or equal to dragons and multiply to see if he can beat dragosns true if yes false if no
+
+}
+

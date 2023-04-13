682012f168088e188a655fb1c5aa007b19ea5e89
Khadir | FsDevKD <125905976+FSDevKD@users.noreply.github.com>
2023-04-13 06:55:34 -0400

    Create makeUppercase.js

---
+++ makeUppercase.js
@@ -0,0 +1,21 @@
+/*
+
+*/
+/*
+
+
+Write a function which converts the input string to uppercase.
+ttps://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/
+
+
+*/
+
+function makeUpperCase(str) {
+
+    return str.toUpperCase(); // returns str to uppercase 
+    
+    // if i was targeting 1st letter it would be 
+    // return str.charAt(0).toUpperCase();
+    
+
+}

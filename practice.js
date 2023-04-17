57b5da530b50f25086fa96118a3ba699d9b3a1a0
Khadir | FsDevKD <125905976+FSDevKD@users.noreply.github.com>
2023-04-17 14:55:21 -0400

    pushing

--- practice.js
+++ practice.js
@@ -101,3 +101,83 @@ function greet(name){
 function greet(name){
     return `Hello, ${name} how are you doing today?`;
 }
+
+function hoopCount (n) {
+   if(n >= 10){
+     return "Great, now move on to tricks"
+   } else {
+     return "Keep at it until you get it"
+   }
+}
+
+function hoopCount (n){
+    if ( n >= 10){
+        return "Great, now move on to tricks";
+    } else {
+        return "Keep at it until you get it";
+    }
+}
+
+function hoopCount(n){
+    if (n >= 10){
+        return "Great, now move on to tricks";
+    } else {
+        return "Keep at it until you get it"
+    }
+}
+
+function hoopCount(n){
+    if(n >= 10){
+        return "Great, now move on to tricks";
+    } else {
+        return "Keep at it until you get it";
+    }
+}
+
+function hoopCount(n){
+    if( n >= 10){
+        return "Great now move on to tricks";
+    } else {
+        return "Keep at it until you get it";
+    }
+}
+
+function hoopCount(n){
+    if( n >= 10){
+        return "Great, now move on to tricks";
+    } else {
+        return "Keep at it until you get it";
+    }
+}
+
+function hoopCount(n){
+    if (n >= 10){
+        return "Great, now move on to tricks";
+    } else {
+        return "Keep at it until you get it";
+    }
+}
+
+function hoopCount(n){
+    if( n >= 10){
+        return "Great, now move on to tricks";
+   } else {
+        return "Keep at it until you get it";
+   }
+}
+
+function hoopCount(n){
+    if ( n >= 10){
+        return "Great, now move on to tricks";
+    } else {
+        return "Keep at it until you get it";
+    }
+}
+
+function hoopCount(n){
+    if(n >= 10){
+        return "Great, now move on to tricks";
+    } else{
+        return "Keep at it until you get it";
+    }
+}
